cf015f19f373a149046d52b9487f02d6710e5b57
DeepakSilaych <deepaksilaych@gmail.com>
2023-02-23 22:12:56 +0530

    added question and answer component

--- Ag_QuizoViz/src/app/app.module.ts
+++ Ag_QuizoViz/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AnswersComponent } from './answers/answers.component';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnswersComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +17,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
